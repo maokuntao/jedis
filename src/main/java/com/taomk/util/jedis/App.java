2782ecb93b369932e452619852830521131b039f
taomk <taomk@yonyou.com>
2016-07-21 16:23:36 +0800

    init
    jedis学习

---
+++ src/main/java/com/taomk/util/jedis/App.java
@@ -0,0 +1,13 @@
+package com.taomk.util.jedis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
